1f47f63eca25173bfbbb485770a5356711dc503c
Gergő Balogh <geryxyz@gmail.com>
2016-01-23 20:38:43 +0100

    cleaning up demo

--- examples/DemoShow/DemoShow.ino
+++ examples/DemoShow/DemoShow.ino
@@ -6,21 +6,59 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(3, 6, NEO_GRB + NEO_KHZ800);
 void setup() {
 	Serial.begin(9600);
 	pixels.begin();
-	pixels.setBrightness(255);
+	pixels.setBrightness(128);
 	Colors::black.putAll(pixels, 3);
 	pixels.show();
 }
 
+//setting the frame rate for the fadings
 #define PIXEL_FPS 60
 
-void loop() {
-	uint32_t waitBetweenScenario = 3000;
-	uint32_t durationInMillisecond = 3000;
-
+void cleanCut() {
 	Colors::black.putAll(pixels, 3);
 	pixels.show();
-	delay(waitBetweenScenario);
-	
+	delay(1000);
+}
+
+uint32_t durationInMillisecond = 3000;
+
+void loop() {
+	//operation between two color
+	cleanCut();
+	Color yellow = Colors::red + Colors::green; //component wise addition
+	Color magenta = Colors::white - Colors::green; //component wise substraction
+	Color mix = yellow * magenta; //component wise average
+	yellow.put(pixels, 0);
+	magenta.put(pixels, 1);
+	mix.put(pixels, 2);
+	pixels.show();
+	delay(5000);
+
+	//operation between a color and a number
+	cleanCut();
+	Color lightMagenta = magenta * 1.0f;
+	Color midMagenta = magenta * .25f;
+	Color darkMagenta = magenta * .05f;
+	lightMagenta.put(pixels, 0);
+	midMagenta.put(pixels, 1);
+	darkMagenta.put(pixels, 2);
+	pixels.show();
+	delay(5000);
+
+	//showing color on all pixels
+	cleanCut();
+	(Colors::red * Colors::green).putAll(pixels, 3);
+	pixels.show();
+	delay(1000);
+	(Colors::red * Colors::blue).putAll(pixels, 3);
+	pixels.show();
+	delay(1000);
+	(Colors::blue * Colors::green).putAll(pixels, 3);
+	pixels.show();
+	delay(1000);
+
+	//single pixel (color) fading
+	cleanCut();
 	ColorFade redToGreen(Colors::red, durationInMillisecond, Colors::green);
 	ColorFade greenToBlue(Colors::green, durationInMillisecond, Colors::blue);
 	ColorFade blueToRed(Colors::blue, durationInMillisecond, Colors::red);
@@ -28,20 +66,16 @@ void loop() {
 	greenToBlue.play(pixels, false, 0);
 	blueToRed.play(pixels, false, 0);
 
-	Colors::black.putAll(pixels, 3);
-	pixels.show();
-	delay(waitBetweenScenario);
-	
+	//back and forth mode for fading
+	cleanCut();	
 	ColorFade nino(Colors::red, 500, Colors::blue);
 	for (uint8_t i = 0; i < 5; i++) {
 		bool backAndForth = true;
 		nino.play(pixels, backAndForth, 0);
 	}
 
-	Colors::black.putAll(pixels, 3);
-	pixels.show();
-	delay(waitBetweenScenario);
-
+	//multi pixel (color) fading
+	cleanCut();
 	Color froms[3] = {
 		Colors::red,
 		Colors::white,
